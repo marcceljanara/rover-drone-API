cc60632c81b96f65e12ac7e2361623caa91097ee
Marccel Janara <nengahyuichiro34@gmail.com>
2025-01-23 12:45:43 +0700

    change call mqtt service from createserver to app to fixing bug

--- src/http/server.js
+++ src/http/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import '../services/cron/CleanExpiredRental.js';
-import '../services/mqtt/SensorsService.js';
 
 // plugin
 import usersPlugin from '../api/users/index.js';
