4059ba4622a2cda94d0d3ee1241abe606d334d0a
Marccel Janara <nengahyuichiro34@gmail.com>
2025-01-30 16:37:26 +0700

    create table reports and edit type data column amount in table device to "DECIMAL(15,2)"

---
+++ migrations/1738228738415_create-table-reports.cjs
@@ -0,0 +1,45 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('reports', {
+    id: {
+      type: 'VARCHAR(23)',
+      primaryKey: true,
+    },
+    user_id: {
+      type: 'VARCHAR(22)',
+      notNull: true,
+      references: '"users"',
+      onDelete: 'SET NULL',
+    },
+    total_transactions: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    total_amount: {
+      type: 'DECIMAL(15, 2)',
+      notNull: true,
+    },
+    report_date: {
+      type: 'TIMESTAMP',
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('reports');
+};
